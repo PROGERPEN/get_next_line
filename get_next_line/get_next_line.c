e5e54a3ba3bb5fac0b72fc9b2a51d1de9eddd397
Falmer gloomlurker Khrazz <fkhrazz@e2r12p5.21-school.ru>
2018-12-11 17:56:24 +0300

    KEK

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fkhrazz <fkhrazz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 13:16:33 by fkhrazz           #+#    #+#             */
-/*   Updated: 2018/12/09 15:25:40 by fkhrazz          ###   ########.fr       */
+/*   Updated: 2018/12/11 17:51:40 by fkhrazz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ int check_static(char **static_line, char **line)
 	{
 		*k = '\0';
 		*line = ft_strdup(*static_line);
+		new_static = ft_strdup((k + 1));
 		ft_memdel((void *) static_line);
-		new_static = strdup((k + 1));
 		*static_line = new_static;
 	}
 	return (1);
@@ -42,73 +42,152 @@ int check_static(char **static_line, char **line)
 
 int		po_buff(char *buff, char **static_line, int fd, int *ret)
 {
+	char *new_line;
+
 	while ((*ret = read (fd, buff, BUFF_SIZE)))
 	{
+		buff[*ret] = '\0';
 		if (*static_line)
 		{
+			new_line = *static_line;
 			*static_line = ft_strjoin(*static_line, buff);
+			free(new_line);
+			new_line = NULL;
 			return (1);
 		}
 		else
 		{
+			new_line = *static_line;
 			*static_line = ft_strdup(buff);
+			free(new_line);
+			new_line = NULL;
 			return (1);
 		}
 	}
 	return (0);
 }
 
+int	static_exist(int fd, char *buff, char **static_line, char **line, int *ret)
+{
+	while (1)
+	{
+		if (check_static(static_line, line))
+		{
+			return (1);
+		}
+		po_buff(buff, static_line, fd, ret);
+ 		if ((!(*ret)) && **static_line != '\0')	
+		{
+			*line = ft_strdup(*static_line);
+			free(*static_line);
+			free(buff);
+			buff = NULL;
+			*static_line = NULL; 
+			return (1);
+		}
+		if ((*ret) == -1)
+		{
+			//free(buff);
+			return (-1);
+		}
+		if (!(*ret))
+			return (0);
+	}
+	return (0);
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	static char *static_line = 0;
+	static char *static_line;
 	char *buff;
 	int k;
 	int ret;
 	ret = 0;
+	if (!line)
+	 return (-1);
 	buff = ft_strnew(BUFF_SIZE);
 	if (!static_line)
 	{
 		static_line = ft_strnew(BUFF_SIZE);
-		read(fd, static_line, BUFF_SIZE);
-	}
-	while (1)
-	{
-		if (check_static(&static_line, line))
-			break ;
 		k = po_buff(buff, &static_line, fd, &ret);
-		if ((!ret) && *static_line != '\0')	
+		if (!ret)
 		{
-			*line = strdup(static_line);
-			return (1);
+			free(static_line);
+			free(buff);
+			buff = NULL;
+			static_line = NULL;
+		 	return (0);
+		}
+		if (ret == -1)
+		{
+			free(static_line);
+			free(buff);
+			return (-1);
 		}
 	}
-
+	if (static_exist(fd, buff, &static_line, line, &ret) == 1)
+		return (1);
+	if (!static_exist(fd, buff, &static_line, line, &ret))
+		return (0);
+	if (static_exist(fd, buff, &static_line, line, &ret) == -1)
+		return (-1);
+	// while (1)
+	// {
+	// 	if (check_static(&static_line, line))
+	// 	{
+	// 		return (1);
+	// 	}
+	// 	k = po_buff(buff, &static_line, fd, &ret);
+	// 	if ((!ret) && *static_line != '\0')	
+	// 	{
+	// 		*line = ft_strdup(static_line);
+	// 		free(static_line);
+	// 		free(buff);
+	// 		buff = NULL;
+	// 		static_line = NULL;
+	// 		return (1);
+	// 	}
+	// 	if (ret == -1)
+	// 	{
+	// 		free(buff);
+	// 		return (-1);
+	// 	}
+	// 	if (!ret)
+	// 		break ;
+	// }
+	// free(buff);
 	return (0);
 }
 
 
 
-/*int		main(int argc, char **argv)
-{
-	int fd = open(argv[1], O_RDONLY);
-	char *line = NULL;
-	free(line);
-	line = (char *)malloc(sizeof(char) * 30);
-	argc = 0;
-	    printf("ARGV: %s\n", *argv);
-	get_next_line(fd, &line);
-	printf("line: %s\n", line);
-	free(line);
-	line = (char *)malloc(sizeof(char) * 30);
-	get_next_line(fd, &line);
-	//printf("%d\n", get_next_line(fd, &line));
-	printf("line: %s\n", line);
-	free(line);
-	line = NULL;
-	line = (char *)malloc(sizeof(char) * 30);
-	get_next_line(fd, &line);
-	printf("line: %s\n", line);
-	return (0);
-
-}*/
+// int		main(int argc, char **argv)
+// {
+// 	int fd = 0;
+// 	fd = open(argv[1], O_RDONLY);
+// 	char *line = NULL;
+// 	//free(line);
+// 	//line = (char *)malloc(sizeof(char) * 30);
+// 	argc = 0;
+// 	    printf("ARGV: %s\n", *argv);
+// //	get_next_line(fd, &line);
+// 	//printf("line: %s\n", line);
+// //	//free(line);
+// 	//line = (char *)malloc(sizeof(char) * 30);
+// 	//get_next_line(fd, &line);
+// 	//printf("%d\n", get_next_line(fd, &line));
+// 	//printf("line: %s\n", line);
+// 	//free(line);
+// 	//line = NULL;
+// 	//line = (char *)malloc(sizeof(char) * 200);
+//     int k;
+// 	while ((k = get_next_line(fd, &line) )== 1)
+// 		//get_next_line(fd, &line);
+// 	{
+// 		printf("%d\n", k);
+// 		printf("%s\n", line);
+// 		//free(line);
+// 	}
+// 	printf("%d\n", k);
+// 	return (0);
+// }
